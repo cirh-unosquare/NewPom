9808388229cae93bb21f9c50a71dd364e85d979c
cesar rodriguez <cesar.rodriguez@unosquare.com>
2024-02-06 22:58:53 -0600

    creating json file and model

---
+++ Model/Searches.cs
@@ -0,0 +1,11 @@
+namespace NewPom.Model
+{
+    public class Searches
+    {
+        public string searchOne { get; set; }
+        public string searchTwo { get; set; }
+        public string searchThree { get; set; }
+        public string searchFour { get; set; }
+        public string searchFive { get; set; }
+    }
+}
